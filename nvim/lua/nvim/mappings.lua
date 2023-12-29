b92f9fe1a956cd602dea6eb4958541dfd1ed66cb
Nico Glas <info@nicoglas.com>
2023-12-29 18:23:35 +0100

    feat(nvim): updated terminal mappings
    using alt-key for window management now

--- nvim/lua/nvim/mappings.lua
+++ nvim/lua/nvim/mappings.lua
@@ -3,14 +3,14 @@ vim.keymap.set("n", "<leader>w", [[:w<CR>]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- window management
-vim.keymap.set("n", "<M-J>", "<C-w>j")
-vim.keymap.set("n", "<M-H>", "<C-w>h")
-vim.keymap.set("n", "<M-K>", "<C-w>k")
-vim.keymap.set("n", "<M-L>", "<C-w>l")
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
 
-vim.keymap.set("t", "<C-J>", "<C-\\><C-n><C-w>j")
-vim.keymap.set("t", "<C-H>", "<C-\\><C-n><C-w>h")
-vim.keymap.set("t", "<C-K>", "<C-\\><C-n><C-w>k")
-vim.keymap.set("t", "<C-L>", "<C-\\><C-n><C-w>l")
+vim.keymap.set("t", "<M-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<M-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<M-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<M-l>", "<C-\\><C-n><C-w>l")
 
 vim.keymap.set("t", "<C-x>", vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true))
