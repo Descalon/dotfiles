462cfc43f633779f097cf2c00f10e3bb0f5217e9
Nico Glas <info@nicoglas.com>
2023-12-29 13:50:43 +0100

    feat(nvim): added session manager

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,7 +21,7 @@ return {
     cond = not vim.g.vscode,
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-      vim.keymap.set("n", "<leader>ga", function() vim.cmd.Git({"adog"}) end)
+      vim.keymap.set("n", "<leader>ga", function() vim.cmd.Git({ "adog" }) end)
     end
   },
   {
@@ -131,5 +131,37 @@ return {
       vim.keymap.set("n", "<leader>v", function() require("nvterm.terminal").new "vertical" end)
       vim.keymap.set("n", "<leader>h", function() require("nvterm.terminal").new "horizontal" end)
     end,
+  },
+  {
+    "Shatur/neovim-session-manager",
+    cond = not vim.g.vscode,
+    config = function()
+      local Path = require('plenary.path')
+      local config = require('session_manager.config')
+      require('session_manager').setup({
+        sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'),
+        autoload_mode = config.AutoloadMode.CurrentDir,
+        autosave_last_session = true,
+        autosave_ignore_not_normal = true,
+        autosave_ignore_dirs = {},
+        autosave_ignore_filetypes = {
+          'gitcommit',
+          'gitrebase',
+        },
+        autosave_ignore_buftypes = {},
+        autosave_only_in_session = false,
+        max_path_length = 80,
+      })
+      vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+        callback = function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
+              return
+            end
+          end
+          require('session-manager').save_current_session()
+        end
+      })
+    end
   }
 }
