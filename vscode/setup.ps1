abe7d34476cec7afe002ed3a4b9292fe2ebcef07
Nico Glas <info@nicoglas.com>
2023-12-30 03:30:19 +0100

    feat(git): added git configuration

--- vscode/setup.ps1
+++ vscode/setup.ps1
@@ -1,19 +1,10 @@
-$settings = "$PSScriptRoot/settings.json"
-$keybindings = "$PSScriptRoot/keybindings.json"
+$configfile = "$PSScriptRoot/git.config"
 
-$settingsSLPath = Resolve-Path -Path "~/appdata/roaming/Code/User/settings.json"
-$keybindingsSLPath = Resolve-Path -Path "~/appdata/roaming/Code/User/keybindings.json"
+$gitconfigpath = Resolve-Path -Path "~/.gitconfig"
 
 
-if(Test-Path $settingsSLPath){
-    $np = $settingsSLPath.Path -replace ".json",".old.json"
-    Rename-Item -Path $settingsSLPath -NewName $np
+if(Test-Path $gitconfigpath){
+    Rename-Item -Path $gitconfigpath -NewName ".gitconfig.old"
 }
 
-if(Test-Path $keybindingsSLPath){
-    $np = $keybindingsSLPath.Path -replace ".json",".old.json"
-    Rename-Item -Path $keybindingsSLPath -NewName $np
-}
-
-New-Item -ItemType SymbolicLink -Path $settingsSLPath -Target $settings
-New-Item -ItemType SymbolicLink -Path $keybindingsSLPath -Target $keybindings
+New-Item -ItemType SymbolicLink -Path $gitconfigpath -Target $configfile
