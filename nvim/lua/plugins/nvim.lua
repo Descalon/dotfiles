7c8801a676d6350f4ab287e8739ea507b74551a8
Nico Glas <info@nicoglas.com>
2023-12-30 02:50:25 +0100

    feat(nvim): added oil as file browser

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -65,7 +65,6 @@ return {
       require("telescope").load_extension "file_browser"
       vim.keymap.set("n", "<leader>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR><ESC>")
     end
-
   },
   {
     "theprimeagen/harpoon",
