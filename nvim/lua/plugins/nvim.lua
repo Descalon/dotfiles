fb7d0ccbc6aadfd002e5248597fa9f70ec98f473
Nico Glas <info@nicoglas.com>
2023-12-31 01:55:52 +0100

    feat(nvim): added orgmode!

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -160,5 +160,28 @@ return {
         end
       })
     end
+  },
+  {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
+    config = function()
+      require('orgmode').setup_ts_grammar()
+
+      require('orgmode').setup({
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      })
+    end,
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    lazy = false,
+    config = function()
+      require('Comment').setup()
+    end,
   }
 }
