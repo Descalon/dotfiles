c230112e911833753890f71c352bd21bcaec0956
Nico Glas <info@nicoglas.com>
2024-01-23 14:49:20 +0100

    settings(nvim): removed column width
    Going to the latex specific options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,10 +4,6 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
 
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.textwidth = 100
-vim.opt.colorcolumn = "100"
 
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
