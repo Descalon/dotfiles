59d4b9bebc3a7d07e50068557463e49127b4526b
Nico Glas <info@nicoglas.com>
2024-01-24 11:06:06 +0100

    settings(nvim): moved focus keybindings to plugin

--- nvim/lua/nvim/mappings.lua
+++ nvim/lua/nvim/mappings.lua
@@ -12,6 +12,7 @@ end
 
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", [[:w<CR>]], { silent = true })
+vim.keymap.set("n", "<leader>W", [[:wa<CR>]], { silent = true })
 vim.keymap.set("x", "<leader>p", [["_dP]], { silent = true })
 vim.keymap.set("n", "<leader>q", function() quitfn('q') end, { silent = true })
 vim.keymap.set("n", "<leader>Q", function() quitfn('qa') end, { silent = true })
@@ -24,5 +25,6 @@ vim.keymap.set({ "n", "t" }, "<M-k>", function() nav.navigate("k") end)
 vim.keymap.set({ "n", "t" }, "<M-l>", function() nav.navigate("l") end)
 
 vim.keymap.set("t", "<C-x>", vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true))
+
 local focus = require('focus')
 vim.keymap.set("n", "<c-o>", function() focus.split_nicely() end)
