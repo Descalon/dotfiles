3c683d8636f49cf7b93f6e3d91958961897d6ea7
Nico Glas <info@nicoglas.com>
2024-01-28 16:14:05 +0100

    settings(psprofile): added dotenv to pengwin profile

--- psprofile/profiles/pengwinProfile.ps1
+++ psprofile/profiles/pengwinProfile.ps1
@@ -3,4 +3,21 @@ Import-Module "$PSScriptRoot/ProfileTemplate.psm1"
 Set-VIMode
 
 Set-Profile princess
-$env:Term = "xterm-x256color"
+
+$env:TERM = "xterm-x256color"
+$env:PATH = @(
+    "/opt/microsoft/powershell/7"
+    "/home/descalon/.local/share/nvim/mason/bin"
+    "/opt/microsoft/powershell/7"
+    "/home/linuxbrew/.linuxbrew/bin"
+    "/home/linuxbrew/.linuxbrew/sbin"
+    "/usr/local/sbin"
+    "/usr/local/bin"
+    "/usr/sbin"
+    "/usr/bin"
+    "/sbin"
+    "/bin"
+    "/usr/games"
+    "/usr/local/games"
+    "/usr/lib/wsl/lib"
+) -join ":"
