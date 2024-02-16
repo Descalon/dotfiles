f32722021341184d3070b391a3d0f63d59dba1ef
Nico Glas <info@nicoglas.com>
2024-02-16 23:49:09 +0100

    settings(nvim): moved mason back to lspsetup
    lspsetup.lua and the mason configuration are not being loaded in the right order.
    This messed up posh and lua ls'.

--- nvim/lua/lspsetup.lua
+++ nvim/lua/lspsetup.lua
@@ -21,3 +21,43 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end
 })
+
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local default_setup = function(server)
+  require('lspconfig')[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
+
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    "kotlin_language_server",
+    "lua_ls",
+    "omnisharp",
+    "powershell_es",
+  },
+  handlers = {
+    default_setup,
+    lua_ls = function()
+      require('lspconfig').lua_ls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT'
+            },
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            workspace = {
+              library = {
+                vim.env.VIMRUNTIME,
+              }
+            }
+          }
+        }
+      })
+    end
+  },
+})
