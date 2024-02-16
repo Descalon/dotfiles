bd53ee14281173d569edadf2d61a674bce765b52
Nico Glas <info@nicoglas.com>
2024-02-16 21:51:54 +0100

    settings(nvim): renamed `orgmode.lua` to neorg

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-neorg/neorg-telescope" },
+  },
+  config = function()
+    local n = require "neorg"
+    n.setup {
+      load = {
+        ["core.defaults"] = {},               -- Loads default behaviour
+        ["core.integrations.telescope"] = {}, -- Loads default behaviour
+        ["core.summary"] = {},
+        ["core.concealer"] = {},              -- Adds pretty icons to your documents
+        ["core.dirman"] = {                   -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            index = "index.norg",
+            default_workspace = "notes"
+          },
+        },
+      },
+    }
+    vim.keymap.set("n", "<leader>oi", [[:Neorg index<CR>]], { silent = true })
+  end,
+}
